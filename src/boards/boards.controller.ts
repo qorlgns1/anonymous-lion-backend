eea4d5315b57e70c39db30cc9ab8cf04191c5064
qorlgns1 <rlgns0610@gmail.com>
2022-05-10 16:44:05 +0900

    feat: createBoard 구현
    Board에 저장하는 기능 추가

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,5 +1,5 @@
 //명령어: nest g controller boards --no-spec (--no-spec: 테스트코드를 생성하지않는다.)
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Board } from './board.model';
 import { BoardsService } from './boards.service';
 
@@ -10,5 +10,13 @@ export class BoardsController {
   @Get('/')
   getAllBoard(): Board[] {
     return this.boardService.getAllBoards();
+  }
+
+  @Post('/')
+  createBoard(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Board {
+    return this.boardService.createBoard(title, description);
   }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,12 +1,24 @@
 //명령어: nest g service boards --no-spec (--no-spec: 테스트코드를 생성하지않는다.)
 import { Injectable } from '@nestjs/common';
-import { Board } from './board.model';
-
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
 @Injectable()
 export class BoardsService {
   private boards: Board[] = [];
 
   getAllBoards(): Board[] {
     return this.boards;
+  }
+
+  createBoard(title: string, description: string) {
+    const board: Board = {
+      id: uuid(),
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    };
+
+    this.boards.push(board);
+    return board;
   }
 }
