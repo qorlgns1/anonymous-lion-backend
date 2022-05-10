39a3b8d32f1cdc642b7556d9cef33085641b5d65
qorlgns1 <rlgns0610@gmail.com>
2022-05-11 01:53:33 +0900

    feat: custom pipe로 상태값 변경 유효성 체크기능 추가

---
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -0,0 +1,30 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  PipeTransform,
+} from '@nestjs/common';
+import { BoardStatus } from '../board.model';
+
+export class BoardStatusValidationPipe implements PipeTransform {
+  readonly StatusOptions = [BoardStatus.PUBLIC, BoardStatus.PRIVATE];
+
+  transform(value: any, metadata: ArgumentMetadata) {
+    // console.log('value', value);
+    // console.log('metadata', metadata);
+
+    value = value.toUpperCase();
+
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(
+        `상태값 ${value} 는 정상적인 값이 아닙니다. 확인해주세요.`,
+      );
+    }
+
+    return value;
+  }
+
+  private isStatusValid(status: any) {
+    const index = this.StatusOptions.indexOf(status);
+    return index !== -1;
+  }
+}
