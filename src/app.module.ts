a1ab0d2571c88488bb46d0fdc62414110e5ddaa5
qorlgns1 <rlgns0610@gmail.com>
2022-05-11 04:18:06 +0900

    feat: database 연결을 위한 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardsModule } from './boards/boards.module';
+import { typeORMConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [BoardsModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardsModule],
   controllers: [],
   providers: [],
 })
