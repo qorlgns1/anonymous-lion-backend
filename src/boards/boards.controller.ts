bd8d4df6c2101dbcd1bad24caa5694c28990403d
qorlgns1 <rlgns0610@gmail.com>
2022-05-10 15:00:39 +0900

    feat: boardModel 추가

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,13 +1,14 @@
 //명령어: nest g controller boards --no-spec (--no-spec: 테스트코드를 생성하지않는다.)
 import { Controller, Get } from '@nestjs/common';
+import { Board } from './board.model';
 import { BoardsService } from './boards.service';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardService: BoardsService) {}
 
-  @Get()
-  getAllBoard() {
+  @Get('/')
+  getAllBoard(): Board[] {
     return this.boardService.getAllBoards();
   }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,11 +1,12 @@
 //명령어: nest g service boards --no-spec (--no-spec: 테스트코드를 생성하지않는다.)
 import { Injectable } from '@nestjs/common';
+import { Board } from './board.model';
 
 @Injectable()
 export class BoardsService {
-  private boards = [];
+  private boards: Board[] = [];
 
-  getAllBoards() {
+  getAllBoards(): Board[] {
     return this.boards;
   }
 }
