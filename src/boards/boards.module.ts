91b2f6b38283414e9a777cc083d5dc574fcc0c0c
qorlgns1 <rlgns0610@gmail.com>
2022-05-11 04:37:56 +0900

    feat: board-repository 생성 및 설정

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,9 +1,12 @@
 //명령어: nest g module boards
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BoardRepository } from './board.repository';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([BoardRepository])],
   controllers: [BoardsController],
   providers: [BoardsService],
 })
