0a78d8dd6979b9354304447cf2b7c660a118281d
qorlgns1 <rlgns0610@gmail.com>
2022-05-10 12:53:11 +0900

    feat: 임시로 cors 설정

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); //임시로 Cors 해제
   await app.listen(3000);
 }
 bootstrap();
