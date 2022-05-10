9d0035548b00e2b97324fa9530bf0f43516f915a
qorlgns1 <rlgns0610@gmail.com>
2022-05-10 21:22:53 +0900

    feat: dto 만들어서 파라미터 dto로 변경

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -2,6 +2,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Board } from './board.model';
 import { BoardsService } from './boards.service';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
@@ -13,10 +14,7 @@ export class BoardsController {
   }
 
   @Post('/')
-  createBoard(
-    @Body('title') title: string,
-    @Body('description') description: string,
-  ): Board {
-    return this.boardService.createBoard(title, description);
+  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+    return this.boardService.createBoard(createBoardDto);
   }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -2,6 +2,7 @@
 import { Injectable } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
 @Injectable()
 export class BoardsService {
   private boards: Board[] = [];
@@ -10,7 +11,9 @@ export class BoardsService {
     return this.boards;
   }
 
-  createBoard(title: string, description: string) {
+  createBoard(createBoardDto: CreateBoardDto) {
+    const { title, description } = createBoardDto;
+
     const board: Board = {
       id: uuid(),
       title,
